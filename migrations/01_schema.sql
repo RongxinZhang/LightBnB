73d972d648d8399375775d4ea8412ee9b70b03b8
Rongxin Zhang <mrrongxin@gmail.com>
2020-08-20 22:46:59 -0700

    Finish migration schema

---
+++ migrations/01_schema.sql
@@ -0,0 +1,54 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS properties CASCADE;
+DROP TABLE IF EXISTS reservations CASCADE;
+DROP TABLE IF EXISTS property_reviews CASCADE;
+
+CREATE TABLE users
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE properties
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
+  cover_photo_url VARCHAR(255) NOT NULL,
+  cost_per_night INTEGER NOT NULL DEFAULT 0,
+  parking_spaces INTEGER NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
+
+  country varchar(255) NOT NULL,
+  steet varchar(255) NOT NULL,
+  city varchar(255) NOT NULL,
+  province varchar(255) NOT NULL,
+  postcode varchar(255) NOT NULL,
+
+  active BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE reservations
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL
+);
+
+CREATE TABLE property_reviews
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE NOT NULL,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
+);
